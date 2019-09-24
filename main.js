d4a7bbc687cd5dad7ec8830646003524aa0601bb
Jun Izumida <gdoublezero00@gmail.com>
2019-09-24 21:04:24 +0900

    [Support] - Test Release

--- main.js
+++ main.js
@@ -17,10 +17,10 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('build/index.html')
+  mainWindow.loadFile('./index.html')
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
